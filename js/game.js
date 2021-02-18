1172054850dd9729640e532e67ee50a500066196
somebodylol123 <S10194206@connect.np.edu.sg>
2021-02-19 04:53:08 +0800

    Finish game element

--- js/game.js
+++ js/game.js
@@ -1,211 +1,234 @@
 $(document).ready(function() {
 
-    // current question object
-    let currentQuesiton = {
+
+    let currentQuestion = {
         ids: [],
-        answer: [],
-        answerIndex: -1,
+        answers: [],
+        answerIndex: -1
     };
 
-    // current scores variables
     let correctAnswer = 0;
-    let wrongAnswer = 0;
+    let incorrectAnswer = 0;
     let noAnsAnswer = 0;
 
-    //Game object
-    let game = {
-        questionsAsked = 0,
 
-        //API
+    // Game  object.
+    let game = {
+        questionsAsked: 0,
+        // API things
         queryMethod: 'GET',
         queryURL: 'http://pokeapi.co/api/v2/pokemon/',
-        
-        //Game methods
-        selectQuestion: function(){
-            if (this.questionsAsked === 10){
-                console.log('NO FUNCTION YET');
-            }
-            else{
-                currentQuesiton.answer = [];
-                CurrentQuestion.ids = [];
-                this.questionsAsked++
 
+        selectQuestion: function() {
+
+            if (game.questionsAsked === 1) {
+                DOMFunctions.callDOMFunctions('gameComplete');
+            } else {
+                currentQuestion.answers = [];
+                currentQuestion.ids = [];
+                game.questionsAsked++;
                 let idArray = [];
                 while (idArray.length < 4) {
                     let pokeID = Math.ceil(Math.random() * 721);
                     if (idArray.indexOf(pokeID) !== -1) continue;
                     idArray.push(pokeID);
                 }
-                // randomly selects 1 of 4 option as answer
+
                 currentQuestion.answerIndex = Math.floor(Math.random() * 4);
 
                 idArray.forEach(function(element) {
                     // Query Pokeapi
-                    $.ajax(url = this.queryURL + element + '/', method = this.queryMethod)
+                    $.ajax(url = game.queryURL + element + '/', method = game.queryMethod)
                         .done(function(response) {
-                            // Push name and ID to currentQuestion.
                             currentQuestion.answers.push(response.species.name);
                             currentQuestion.ids.push(response.id);
-                            // Check whether to start the question.
-                            this.startQuestion();
+                            game.startQuestion();
                         });
                 });
             }
         },
 
-        startQuestion: function(){
-            if (currentQuestion.ids.length === 4){
+        startQuestion: function() {
+            if (currentQuestion.ids.length === 4) {
                 DOMFunctions.callDOMFunctions(event = 'newQuestion');
                 timer.start();
             }
         },
 
-        isCorrect: function(){
-            if (answer === currentQuestion.answers[currentQuestion.answerIndex]){
+        isCorrect: function(answer) {
+            if (answer === currentQuestion.answers[currentQuestion.answerIndex]) {
                 return true;
-            }
-            else{
+            } else {
                 return false;
             }
         },
 
-        updateScores: function(correct){
-            if (correct === true){
+        updateScores: function(correct) {
+            if (correct === true) {
                 correctAnswer++;
-            }
-            else if(correct === false){
-                wrongAnswer++;
-            }
-            else{
-                noAnsAnswer ++;
+            } else if (correct === false) {
+                incorrectAnswer++;
+            } else {
+                noAnsAnswer++;
             }
         },
-    }
 
-    //DOM functions
+        resetGame: function() {
+            game.questionsAsked = 0;
+            correctAnswer = 0;
+            incorrectAnswer = 0;
+            noAnsAnswer = 0;
+        }
+    };
+
+
+    // DOM functions
     let DOMFunctions = {
-        showTimer : function(){
+        ShowTimer: function() {
             $('.timer').css('display', 'block');
         },
-        showTimeLeft: function(){
+        ShowTimeLeft: function(timeRemaining) {
             $('#timer').text(timeRemaining);
         },
-        showGIF : function(){
-            //hides start button
+        ShowGIF: function() {
             $('.start').hide();
-            $('.timer').css('display','block');
-            $('.loading-image').css('display','block');
+            $('.timer').css('display', 'block');
+            $('.loading-image').css('display', 'block');
         },
-        showQuestion: function(){
-            //Hide loading gif
+        ShowQuestion: function() {
             $('.loading-image').hide();
-            $('.question-display').show();
-            $('.display-options').show();
-            $('.answer-options').show();
+            $('.reset').css('display', 'block');
+            $('.showPokeImg').show();
+            $('.displayOptions').show();
+            $('.answer-options').empty();
+            $('.pokeImg').addClass('silhouette');
+            $('.pokeImg').attr('src', 'image/pokemonImg/' + currentQuestion.ids[currentQuestion.answerIndex] + '.png');
+            $('.pokeImg').attr('alt', 'A mystery Pokemon\'s silhouette');
 
-            //make pokemon image silhouetted
-            $('.poke-image').addclass('silhouette');
-            $('.question-image').attr('src', '/image/' + currentQuestion.ids[currentQuestion.answerIndex] + '.png');
-            $('.question-image').attr('alt', 'pokemon\'s silhouette');
-
-            //adding answer options
-            for (let i = 0; i < currentQuestion.answer.length; i++){
+            for (let i = 0; i < currentQuestion.answers.length; i++) {
                 option = $('<p>');
-                option.text(currentQuestion.answer[i]);
-                option.addclass('text-center option');
-                option.attr('role','button');
-                option.attr('data-name',currentQuestion.answer[i]);
+                option.text(currentQuestion.answers[i]);
+                option.addClass('text-center option');
+                option.attr('role', 'button');
+                option.attr('data-name', currentQuestion.answers[i]);
                 $('.answer-options').append(option);
             }
         },
-        showAnswer: function(correct){
+        ShowAnswer: function(correct) {
             $('.timer').hide();
-            $('.display-options').hide();
-            $('.itsPokemon').show();
+            $('.displayOptions').hide();
+            $('.result').show();
             $('#correct-answer').text('It\'s ' + currentQuestion.answers[currentQuestion.answerIndex] + '!');
-            $('.poke-image').toggleClass('silhouette');
-            $('.poke-image').attr('alt', 'An image of ' + currentQuestion.answers[currentQuestion.answerIndex]);
+            $('.pokeImg').toggleClass('silhouette');
+            $('.pokeImg').attr('alt', 'An image of ' + currentQuestion.answers[currentQuestion.answerIndex]);
             if (correct === true) {
-                $('#right-wrong').text('Correct!');
-            } 
-            else if (correct === false) {
-                $('#right-wrong').text('Incorrect');
-            } 
-            else {
-                $('#right-wrong').text('Time is Up!');
+                $('#right-or-wrong').text('Correct!');
+            } else if (correct === false) {
+                $('#right-or-wrong').text('Incorrect...');
+            } else {
+                $('#right-or-wrong').text('Time\'s Up!');
             }
-            setTimeout(game.selectQuestion, 1500);
+            setTimeout(game.selectQuestion, 1700);
         },
         hideAnswer: function() {
             $('.timer').show();
-            $('itsPokemon').hide();
+            $('.result').hide();
         },
-        showResults: function() {
-            $('poke-image').hide();
-            $('timer').hide();
-            $('.resultBox').show();
-            $('#correctAns').text('Correct: ' + correctAnswer);
-            $('#incorrectAns').text('Incorrect: ' + wrongAnswer);
-            $('#noAns').text('Unanswered: ' + noAnsAnswer);
-            $('.restart').show();
-        },
-        createNewGame: function() {
-            // Hide restart, timer section, and game results.
-            $('.restartBtn').hide();
+        ShowResults: function() {
+            $('.showPokeImg').hide();
             $('.timer').hide();
-            $('.resultBox').hide();
-            // Clear timer text.
-            $('#timer').text('');
-            // Show start button.
-            $('.start').show();
-            // Ensure the first image will be silhouetted
-            $('.poke-images').addClass('silhouette');
+            $('.finalResultBox').show();
+            $('.restart').show();
+            $('#correctAns').text('Correct: ' + correctAnswer);
+            $('#wrongAns').text('Incorrect: ' + incorrectAnswer);
+            $('#noAns').text('Unanswered: ' + noAnsAnswer);
+            $('.reset').hide();
         },
-        callDOMFunctions : function(event,correct, timeRemaining) {
-            // call functions according to state of game
+        ShowNewGame: function() {
+            $('.restart').hide();
+            $('.timer').hide();
+            $('.finalResultBox').hide();
+            $('#timer').text('');
+            $('.start').show();
+            $('.pokeImg').addClass('silhouette');
+            $('.reset').hide();
+        },
+
+
+        callDOMFunctions: function(event, correct, timeRemaining) {
             if (event === 'startGame') {
-                DOMFunctions.showGIF();
+                DOMFunctions.ShowGIF();
             } else if (event === 'newQuestion') {
                 DOMFunctions.hideAnswer();
-                DOMFunctions.showQuestion();
-                DOMFunctions.showTimer();
+                DOMFunctions.ShowQuestion();
+                DOMFunctions.ShowTimer();
             } else if (event === 'countDown') {
-                DOMFunctions.showTimeLeft(timeRemaining);
+                DOMFunctions.ShowTimeLeft(timeRemaining);
             } else if (event === 'questionAnswered') {
-                DOMFunctions.showAnswer(correct);
+                DOMFunctions.ShowAnswer(correct);
             } else if (event === 'gameComplete') {
                 DOMFunctions.hideAnswer();
-                DOMFunctions.showResults();
+                DOMFunctions.ShowResults();
             } else if (event === 'restartClicked') {
-                DOMFunctions.createNewGame();
+                DOMFunctions.ShowNewGame();
             }
         }
     };
-    let timer = {
-        count: 9,
 
-        //countdown timer
-        start: function(){
+    let timer = {
+
+        count: 11,
+
+        start: function() {
             timer.decrement();
             counter = setInterval(timer.decrement, 1000);
         },
 
-        decrement: function(){
+        decrement: function() {
             timer.count--;
-            DOMFunctions.callDOMFunctions(event = 'countDown',correct = null, timeRemaining = timer.count);
-            if (timer.count === 0){
+            DOMFunctions.callDOMFunctions(event = 'countDown', correct = null, timeRemaining = timer.count);
+            if (timer.count === 0) {
                 timer.stop();
                 game.updateScores();
-                DOMFunctions.callDOMFunctions(events = 'questionAnswered', correct = '');
+                DOMFunctions.callDOMFunctions(event = 'questionAnswered', correct = '');
             }
         },
 
-        //reset timer
-        stop: function(){
+        stop: function() {
             clearInterval(counter);
-            timer.count = 9
-        }
-    },
+            timer.count = 11;
+        },
+    };
 
+    // event listeners
+    // start btn click on
+    $('#startBtn').on('click', function() {
+        game.selectQuestion();
+        DOMFunctions.callDOMFunctions(event = 'startGame');
+    });
 
-})
+    // ans is click on.
+    $(document).on('click', '.option', function() {
+        let answerText = $(this).data('name');
+        // Check if the answer is correct.
+        let answerIsCorrect = game.isCorrect(answerText);
+        timer.stop();
+        game.updateScores(answerIsCorrect);
+        DOMFunctions.callDOMFunctions(event = 'questionAnswered', correct = answerIsCorrect);
+    });
+
+    // restart button click on.
+    $('#restartBtn').on('click', function() {
+        game.resetGame();
+        DOMFunctions.callDOMFunctions(event = 'restartClicked');
+    });
+
+    // reset button click on
+    $('#resetBtn').on('click', function() {
+        game.resetGame();
+        DOMFunctions.callDOMFunctions(event = 'restartClicked');
+        $('.showPokeImg').hide();
+        $('.displayOptions').hide();
+        timer.stop();
+    })
+});
